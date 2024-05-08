7cb3b0d64c0f21f9afea07a19b5d02d2fd8ab24a
Daniel Lee <143138207+arcjak@users.noreply.github.com>
2024-05-08 04:37:35 -0700

    Project_2_CPSC_323.cpp

--- Project_2_CPSC_323.cpp
+++ Project_2_CPSC_323.cpp
@@ -1,137 +1,208 @@
 #include <iostream>
 #include <stack>
-#include <unordered_map>
 #include <string>
+#include <vector>
+
 
 using namespace std;
 
-// Define tokens
-enum class Token { ID, PLUS, MULTIPLY, LEFT_PAREN, RIGHT_PAREN, END };
-
-// Define parser states
-enum class State { S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, ACCEPT, ERROR };
-
-// Function to map states to production rules
-string getProductionRule(State state) {
-    switch (state) {
-        case State::S1: return "E → E + T";
-        case State::S2: return "E → T";
-        case State::S3: return "T → T * F";
-        case State::S4: return "T → F";
-        case State::S5: return "F → (E)";
-        case State::S6: return "F → id";
-        default: return "";
-    }
-}
-
-// Function to get token from input character
-Token getToken(char c) {
-    switch (c) {
-        case '+':
-            return Token::PLUS;
-        case '*':
-            return Token::MULTIPLY;
-        case '(':
-            return Token::LEFT_PAREN;
-        case ')':
-            return Token::RIGHT_PAREN;
-        case '$':
-            return Token::END;
-        default:
-            return Token::ID; // Assume any other character is an identifier
-    }
-}
-// Parsing table
-unordered_map<State, unordered_map<Token, pair<State, int>>> parsingTable = {
-    {State::S0, {{Token::ID, {State::S5, -1}}, {Token::PLUS, {State::S4, -1}}, {Token::MULTIPLY, {State::ERROR, 1}}, {Token::LEFT_PAREN, {State::S1, -1}}, {Token::RIGHT_PAREN, {State::ERROR, 3}}, {Token::END, {State::ERROR, 3}}}},
-    {State::S1, {{Token::ID, {State::S5, -1}}, {Token::PLUS, {State::S4, -1}}, {Token::MULTIPLY, {State::ERROR, 1}}, {Token::LEFT_PAREN, {State::S1, -1}}, {Token::RIGHT_PAREN, {State::ERROR, 3}}, {Token::END, {State::ERROR, 3}}}},
-    {State::S2, {{Token::ID, {State::ERROR, 2}}, {Token::PLUS, {State::ERROR, 2}}, {Token::MULTIPLY, {State::S7, 1}}, {Token::LEFT_PAREN, {State::ERROR, 2}}, {Token::RIGHT_PAREN, {State::ERROR, 2}}, {Token::END, {State::ERROR, 2}}}},
-    {State::S3, {{Token::ID, {State::ERROR, 4}}, {Token::PLUS, {State::ERROR, 4}}, {Token::MULTIPLY, {State::S8, 1}}, {Token::LEFT_PAREN, {State::ERROR, 4}}, {Token::RIGHT_PAREN, {State::ERROR, 4}}, {Token::END, {State::ERROR, 4}}}},
-    {State::S4, {{Token::ID, {State::S5, -1}}, {Token::PLUS, {State::S4, -1}}, {Token::MULTIPLY, {State::S8, -2}}, {Token::LEFT_PAREN, {State::S1, -1}}, {Token::RIGHT_PAREN, {State::ERROR, 3}}, {Token::END, {State::ERROR, 3}}}},
-    {State::S5, {{Token::ID, {State::S6, -1}}, {Token::PLUS, {State::ERROR, 5}}, {Token::MULTIPLY, {State::ERROR, 5}}, {Token::LEFT_PAREN, {State::S1, -1}}, {Token::RIGHT_PAREN, {State::ERROR, 5}}, {Token::END, {State::ERROR, 5}}}},
-    {State::S6, {{Token::ID, {State::ERROR, 9}}, {Token::PLUS, {State::ERROR, 9}}, {Token::MULTIPLY, {State::ERROR, 9}}, {Token::LEFT_PAREN, {State::ERROR, 9}}, {Token::RIGHT_PAREN, {State::ERROR, 9}}, {Token::END, {State::ERROR, 9}}}},
-    {State::S7, {{Token::ID, {State::S5, -1}}, {Token::PLUS, {State::ERROR, 6}}, {Token::MULTIPLY, {State::ERROR, 6}}, {Token::LEFT_PAREN, {State::S1, -1}}, {Token::RIGHT_PAREN, {State::ERROR, 6}}, {Token::END, {State::ERROR, 6}}}},
-    {State::S8, {{Token::ID, {State::S5, -1}}, {Token::PLUS, {State::ERROR, 7}}, {Token::MULTIPLY, {State::ERROR, 7}}, {Token::LEFT_PAREN, {State::S1, -1}}, {Token::RIGHT_PAREN, {State::ERROR, 7}}, {Token::END, {State::ERROR, 7}}}},
-    {State::S9, {{Token::ID, {State::ERROR, 10}}, {Token::PLUS, {State::ERROR, 10}}, {Token::MULTIPLY, {State::ERROR, 10}}, {Token::LEFT_PAREN, {State::ERROR, 10}}, {Token::RIGHT_PAREN, {State::ERROR, 10}}, {Token::END, {State::ERROR, 10}}}},
-    {State::S10, {{Token::ID, {State::S5, -1}}, {Token::PLUS, {State::ERROR, 8}}, {Token::MULTIPLY, {State::ERROR, 8}}, {Token::LEFT_PAREN, {State::S1, -1}}, {Token::RIGHT_PAREN, {State::ERROR, 8}}, {Token::END, {State::ERROR, 8}}}},
-    {State::S11, {{Token::ID, {State::ERROR, 11}}, {Token::PLUS, {State::ERROR, 11}}, {Token::MULTIPLY, {State::ERROR, 11}}, {Token::LEFT_PAREN, {State::ERROR, 11}}, {Token::RIGHT_PAREN, {State::ERROR, 11}}, {Token::END, {State::ERROR, 11}}}}
-};
-
-
-
-// Function to parse input string
-bool parseInput(const string& input) {
-    stack<State> stateStack;
-    stateStack.push(State::S0); // Initial state
-    string remainingInput = input + "$";
-    size_t inputIndex = 0;
+template<typename T> class Parsing {
+public:
+// Function to parse the input string
+bool parse_Input(string input) {
+    vector<int> stk;
+    stk.push_back(0);  // Push initial state
+    
+    // Parsing table
+    int parsingTable[12][9] = {
+        {5, 0, 0, 4, 0, 0},
+        {0, 6, 0, 0, 0, -7},
+        {0, -2, 7, 0, -2, -2},
+        {0, -4, -4, 0, -4, -4},
+        {5, 0, 0, 4, 0, 0},
+        {0, -6, -6, 0, -6, -6},
+        {5, 0, 0, 4, 0, 0},
+        {5, 0, 0, 4, 0, 0},
+        {0, 6, 0, 0, 11, -3},
+        {0, -1, 7, 0, -1, -1},
+        {0, -3, -3, 0, -3, -3},
+        {0, -5, -5, 0, -5, -5}
+    };
+    int gotoTable[12][3] = {
+        {1, 2, 3},
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+        {8, 2, 3},
+        {0, 0, 0},
+        {0, 9, 3},
+        {0, 0, 10},
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
 
     cout << "Input: " << input << endl;
     cout << "Stack:" << endl;
-    cout << "Step\tStack\t\t\tInput\tAction" << endl;
+    cout << "Step\tStack <--\tInput  \t\tTable\t\tAction" << endl;
+    int count = 0;
+    while (count <= input.size()-1) {
+        
+        int currentState = stk.back();
+        
+        int currentSymbol;
+        cout << "-" << "\t";
+        for (int i =0; i < stk.size(); i++)
+        {
+            cout << stk[i] << " ";
+        }
+        cout << "\t\t" << input.substr(count) << "\t\t";
 
-    while (!stateStack.empty()) {
-        State currentState = stateStack.top();
-        char nextInput = remainingInput[inputIndex];
-        Token nextToken = getToken(nextInput);
-
-        if (parsingTable[currentState].find(nextToken) != parsingTable[currentState].end()) {
-            pair<State, int> nextState = parsingTable[currentState][nextToken];
-
-            // Display parsing step along with the stack content
-            cout << inputIndex << "\t";
-            stack<State> tempStack = stateStack;
-            while (!tempStack.empty()) {
-                cout << static_cast<int>(tempStack.top()) << " ";
-                tempStack.pop();
-            }
-            cout << "\t\t" << remainingInput.substr(inputIndex) << "\t";
-
-            // Apply action
-            switch (nextState.second) {
-                case -1:
-                    stateStack.push(nextState.first);
-                    inputIndex++;
-                    cout << "Shift " << static_cast<int>(nextState.first) << endl;
-                    break;
-                case 1:
-                    stateStack.push(nextState.first);
-                    cout << "Reduce " << getProductionRule(nextState.first) << endl;
-                    break;
-                case 0:
-                    cout << "Accept" << endl;
-                    return true;
-                default:
-                    cout << "Error" << endl;
-                    return false;
-            }
+        // Check for 'id' token
+        if (input[count] == 'i' && count + 1 < input.size() && input[count + 1] == 'd') {
+            currentSymbol = 0; // 'id' token
+            count++;
         } else {
-            cout << inputIndex << "\t";
-            stack<State> tempStack = stateStack;
-            while (!tempStack.empty()) {
-                cout << static_cast<int>(tempStack.top()) << " ";
-                tempStack.pop();
+            switch (input[count]) {
+                case '+': currentSymbol = 1; break;
+                case '*': currentSymbol = 2; break;
+                case '(': currentSymbol = 3; break;
+                case ')': currentSymbol = 4; break;
+                case '$': currentSymbol = 5; break;
+                default: return false; // Invalid input
             }
-            cout << "\t\t" << remainingInput.substr(inputIndex) << "\t";
-            cout << "Error: No transition for state " << static_cast<int>(currentState) << " and token " << static_cast<int>(nextToken) << endl;
+        }
+
+        int action = parsingTable[currentState][currentSymbol];
+
+
+        // Print stack contents
+        
+
+        if (action == 0) {
+            cout << "Invalid input";
             return false;
         }
+        else if (action == -1) {
+            cout << "R" << parsingTable[currentState][currentSymbol]/-1 << "\t\t";
+            cout << "Reduce by E -> E + T";
+            stk.pop_back();
+            stk.pop_back();
+            stk.pop_back();
+
+            stk.push_back(gotoTable[stk[stk.size()-1]][0]);
+
+
+        } else if (action == -2) {
+            cout << "R" << parsingTable[currentState][currentSymbol]/-1 << "\t\t";
+            cout << "Reduce by E -> T";
+            stk.pop_back();
+            stk.push_back(gotoTable[stk[stk.size()-1]][0]);
+
+
+        } else if (action == -3) {
+            cout << "R" << parsingTable[currentState][currentSymbol]/-1 << "\t\t";
+            cout << "Reduce by T -> T * F";
+            stk.pop_back(); stk.pop_back(); stk.pop_back();
+            stk.push_back(2);
+
+
+        } else if (action == -4) {
+            cout << "R" << parsingTable[currentState][currentSymbol]/-1 << "\t\t";
+            cout << "Reduce by T -> F";
+            stk.pop_back(); 
+            stk.push_back(gotoTable[stk[stk.size()-1]][1]);
+        } else if (action == -5) {
+            cout << "R" << parsingTable[currentState][currentSymbol]/-1 << "\t\t";
+            cout << "Reduce by F -> (E)";
+            stk.pop_back();
+            stk.pop_back();
+            stk.pop_back();
+
+            stk.push_back(gotoTable[stk[stk.size()-1]][2]);
+
+        } else if (action == -6) {
+            cout << "R" << parsingTable[currentState][currentSymbol]/-1 << "\t\t";
+            cout << "Reduce by F -> id" <<  gotoTable[stk[stk.size()-1]][2];
+            stk.pop_back();
+            stk.push_back(gotoTable[stk[stk.size()-1]][2]);
+            cout << stk[stk.size()-1];
+        }
+     else if (action == 1) {
+            cout << "S" << parsingTable[currentState][currentSymbol] << "\t\t";
+            cout << "Shift to state " << parsingTable[currentState][currentSymbol];
+            stk.push_back(parsingTable[currentState][currentSymbol]);
+                        count++;
+
+        } else if (action == 2) {
+            cout << "S" << parsingTable[currentState][currentSymbol] << "\t\t";
+            cout << "Shift to state " << parsingTable[currentState][currentSymbol];
+            stk.push_back(parsingTable[currentState][currentSymbol]);
+                        count++;
+
+        } else if (action == 3) {
+            cout << "S" << parsingTable[currentState][currentSymbol] << "\t\t";
+            cout << "Shift to state " << parsingTable[currentState][currentSymbol];
+            stk.push_back(parsingTable[currentState][currentSymbol]);
+                        count++;
+
+        }else if (action == 4) {
+            cout << "S" << parsingTable[currentState][currentSymbol] << "\t\t";
+            cout << "Shift to state " << parsingTable[currentState][currentSymbol];
+            stk.push_back(parsingTable[currentState][currentSymbol]);
+                        count++;
+
+        }else if (action == 5) {
+            cout << "S" << parsingTable[currentState][currentSymbol] << "\t\t";
+            cout << "Shift " << parsingTable[currentState][currentSymbol];
+            stk.push_back(parsingTable[currentState][currentSymbol]);
+                        count++;
+
+        }else if (action == 6) {
+            cout << "S" << parsingTable[currentState][currentSymbol] << "\t\t";
+            cout << "Shift to state " << parsingTable[currentState][currentSymbol];
+            stk.push_back(parsingTable[currentState][currentSymbol]);
+                        count++;
+
+        }else if (action == 7) {
+            cout << "S" << parsingTable[currentState][currentSymbol] << "\t\t";
+            cout << "Shift to state " << parsingTable[currentState][currentSymbol];
+            stk.push_back(parsingTable[currentState][currentSymbol]);
+            count++;
+
+        }else if (action == -7) {
+            cout << "Accept";
+            return true;
+        }else if (action == 0) {
+            cout << "Error";
+            return false;
+        } else {
+            cout << "S" << action << "\t\t";
+            cout << "Shift to state " << action;
+            stk.push_back(action);
+                        count++;
+
+        }
+
+        cout << endl;
     }
 
     return false;
 }
+};
 
 int main() {
-    // Test cases
-    string inputs[] = {"(id+id)*id$", "id*id", "(id*)$"};
-    for (const string& input : inputs) {
-        if (parseInput(input)) {
-            cout << "Output: String is accepted." << endl;
-        } else {
-            cout << "Output: String is not accepted." << endl;
-        }
-        cout << endl;
+    // Test input strings
+    string inputs[] = {"(id+id)*id$", "id*id$", "(id*)$"};
+    int numInputs = sizeof(inputs) / sizeof(inputs[0]);
+    Parsing<string> pf1;
+
+    for (int i = 0; i < numInputs; ++i) {
+        bool result = pf1.parse_Input(inputs[i]);
+        cout << (result ? "\nAccepted" : "\nRejected") << endl << endl;
     }
+    
 
     return 0;
 }
